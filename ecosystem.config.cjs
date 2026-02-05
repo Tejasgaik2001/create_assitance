530efa05736c3a86f3efb7b5572cceac1cc52574
prathmesh-ops <prathmesh.onpoint@gmail.com>
2026-02-05 13:23:42 +0530

    Build

---
+++ ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+
+  apps: [
+
+    {
+
+      name: "createassistant-website",
+
+      script: "serve",
+
+      env: {
+
+        PM2_SERVE_PATH: "./dist",
+
+        PM2_SERVE_PORT: 8080,
+
+        PM2_SERVE_SPA: "true",
+
+        PM2_SERVE_HOMEPAGE: "/index.html"
+
+      }
+
+    }
+
+  ]
+
+}
+
+ 
