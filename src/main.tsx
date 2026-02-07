f915b223736bfb00b90d39f57b6d19fcc570aac1
prathmesh-ops <prathmesh.onpoint@gmail.com>
2026-02-08 00:02:14 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,4 +8,12 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root")!;
+
+// Safari/mobile: disable concurrent scheduling to reduce CPU contention with video decoding
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isMobile = /iphone|ipad|ipod|android/i.test(navigator.userAgent);
+
+createRoot(container, {
+  unstable_concurrentUpdatesByDefault: !(isSafari || isMobile),
+} as any).render(<App />);
